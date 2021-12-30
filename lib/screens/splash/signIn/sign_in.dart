49cc9a4adec655101457b32ca73c464f5ba76ed7
sumanth <sumanth.badugu07@gmail.com>
2021-12-30 23:43:14 +0530

    updated

--- lib/screens/splash/signIn/sign_in.dart
+++ lib/screens/splash/signIn/sign_in.dart
@@ -6,7 +6,7 @@ class SignInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('                      Sign In'),
+        title: Center(child: Text('Sign In')),
       ),
     );
   }
